166a4b986ff5738abb63aea4bdd5a38cec7cd41c
Paul Calzado <pafcalzado@gmail.com>
2021-12-31 15:25:13 +0800

    Add logic to randomize background color and increment it by 10% on each mouse hover

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ function createGrid(grid = 16) {
 
 	if (div.length) {
 		Array.from(div).forEach((d) => gridContainer.removeChild(d));
+		clearGrid();
 	}
 
 	for (let i = 0; i < DIV_NUMBER; i++) {
@@ -32,11 +33,30 @@ function createGrid(grid = 16) {
 	}
 }
 
-createGrid(32);
-
 function drawHandler(event) {
 	const div = event.target;
+	const bgColor = div.style.backgroundColor.match(/\d+/g);
+	if (!bgColor) {
+		div.style.backgroundColor = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+	} else {
+		div.style.backgroundColor = `rgb(${incrementColor(
+			bgColor[0]
+		)},${incrementColor(bgColor[1])},${incrementColor(bgColor[2])})`;
+	}
 	div.classList.add('active');
+}
+
+function incrementColor(color) {
+	let parsedColor = parseInt(color);
+	let newColor;
+
+	if (parsedColor <= 25) {
+		newColor = 0;
+	} else if (parsedColor <= 255 || parsedColor >= 25) {
+		newColor = parsedColor - 25;
+	}
+
+	return newColor.toString();
 }
 
 function clearGrid() {
@@ -45,13 +65,22 @@ function clearGrid() {
 }
 
 function clickHandler() {
-	const para = document.createElement('p');
-	let newGrid = prompt('Enter grid size: ');
-	if (!Number(newGrid) || (newGrid > 100 && newGrid > 0)) {
+	let newGrid = parseInt(prompt('Enter grid size: '));
+	if (!Number(newGrid) || (newGrid > 100 && newGrid < 0)) {
 		para.textContent = 'Please enter a valid number.';
 		container.appendChild(para);
 		newGrid = prompt('Enter grid size: ');
 	}
-	clearGrid();
+
 	createGrid(newGrid);
+}
+
+createGrid();
+
+function randomNumber() {
+	return Math.floor(Math.random() * 255);
+}
+
+function randomColorGenerator(element) {
+	element.style.backgroundColor = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
 }
