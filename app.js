77470c02899ff7bbf98307628f491f7b688f4712
Paul Calzado <pafcalzado@gmail.com>
2021-12-31 18:18:33 +0800

    Add and refactor logic for added features

--- app.js
+++ app.js
@@ -1,20 +1,86 @@
 const container = document.getElementById('container');
+const btnContainer = document.createElement('div');
+const gridPanelContainer = document.createElement('div');
 const gridContainer = document.createElement('div');
 const clearBtn = document.createElement('button');
+const originalBtn = document.createElement('button');
+const eraseBtn = document.createElement('button');
+const randomColorBtn = document.createElement('button');
+const gradientBtn = document.createElement('button');
+const randomGradientBtn = document.createElement('button');
+const sliderContainer = document.createElement('div');
+const slider = document.createElement('input');
+const sliderValue = document.createElement('p');
 
 const GRID_CONTAINER_SIZE = 512;
 
-container.appendChild(clearBtn);
-container.appendChild(gridContainer);
+const ORIGINAL_MODE = 'Black&White';
+const RANDOMIZE_MODE = 'RandomizeColor';
+const GRADIENT_MODE = 'GradientColor';
+const RANDOMIZE_GRADIENT_MODE = 'RandomizeGradientColor';
+const ERASE_MODE = 'Erase';
+
+let drawMode = ORIGINAL_MODE;
+
+container.appendChild(btnContainer);
+container.appendChild(gridPanelContainer);
+btnContainer.appendChild(clearBtn);
+btnContainer.appendChild(originalBtn);
+btnContainer.appendChild(eraseBtn);
+btnContainer.appendChild(randomColorBtn);
+btnContainer.appendChild(gradientBtn);
+btnContainer.appendChild(randomGradientBtn);
+gridPanelContainer.appendChild(sliderContainer);
+gridPanelContainer.appendChild(gridContainer);
+sliderContainer.appendChild(slider);
+sliderContainer.appendChild(sliderValue);
 
 gridContainer.classList.add('grid-container');
-clearBtn.textContent = 'Clear';
+btnContainer.classList.add('btn-container');
+gridPanelContainer.classList.add('grid-panel-container');
+
+originalBtn.classList.add('btn');
+eraseBtn.classList.add('btn');
 clearBtn.classList.add('btn');
-clearBtn.addEventListener('click', clickHandler);
+randomColorBtn.classList.add('btn');
+gradientBtn.classList.add('btn');
+randomGradientBtn.classList.add('btn');
+
+sliderContainer.classList.add('slider-container');
+slider.classList.add('slider');
+sliderValue.classList.add('slider-value');
+
+slider.type = 'range';
+slider.min = 1;
+slider.max = 100;
+slider.value = 16;
+slider.step = 1;
+
+sliderValue.textContent = `${slider.value} x ${slider.value}`;
+
+clearBtn.textContent = 'Clear';
+originalBtn.textContent = 'Normal';
+eraseBtn.textContent = 'Eraser';
+randomColorBtn.textContent = 'Randomize';
+gradientBtn.textContent = 'Gray Gradient';
+randomGradientBtn.textContent = 'Random Gradient';
+
+clearBtn.addEventListener('click', clearGrid);
+originalBtn.addEventListener('click', () => drawModeHandler(ORIGINAL_MODE));
+eraseBtn.addEventListener('click', () => drawModeHandler(ERASE_MODE));
+randomColorBtn.addEventListener('click', () => drawModeHandler(RANDOMIZE_MODE));
+gradientBtn.addEventListener('click', () => drawModeHandler(GRADIENT_MODE));
+randomGradientBtn.addEventListener('click', () =>
+	drawModeHandler(RANDOMIZE_GRADIENT_MODE)
+);
+slider.addEventListener('change', (event) => resetGrid(event.target.value));
+slider.addEventListener('mousemove', (event) =>
+	updateSliderValueText(event.target.value)
+);
 
 function createGrid(grid = 16) {
-	const GRID_SIZE = GRID_CONTAINER_SIZE / grid;
-	const DIV_NUMBER = grid ** 2;
+	const gridSize = GRID_CONTAINER_SIZE / grid;
+	const divNumber = grid ** 2;
 
 	const div = document.querySelectorAll('.grid');
 
@@ -23,27 +89,105 @@ function createGrid(grid = 16) {
 		clearGrid();
 	}
 
-	for (let i = 0; i < DIV_NUMBER; i++) {
+	for (let i = 0; i < divNumber; i++) {
 		const div = document.createElement('div');
 		div.classList.add('grid');
-		div.style.width = `${GRID_SIZE}px`;
-		div.style.height = `${GRID_SIZE}px`;
+		div.style.width = `${gridSize}px`;
+		div.style.height = `${gridSize}px`;
 		div.addEventListener('mouseenter', drawHandler);
 		gridContainer.appendChild(div);
 	}
 }
 
+function drawModeHandler(mode) {
+	drawMode = mode;
+	console.log(drawMode);
+}
+
 function drawHandler(event) {
 	const div = event.target;
-	const bgColor = div.style.backgroundColor.match(/\d+/g);
-	if (!bgColor) {
-		div.style.backgroundColor = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+
+	switch (drawMode) {
+		case ORIGINAL_MODE:
+			blackAndWhiteHandler(div, 'black');
+			break;
+		case ERASE_MODE:
+			blackAndWhiteHandler(div, 'white');
+			break;
+		case RANDOMIZE_MODE:
+			randomizeColorHandler(div);
+			break;
+		case GRADIENT_MODE:
+			gradientColorHandler(div);
+			break;
+		case RANDOMIZE_GRADIENT_MODE:
+			randomGradientColorHandler(div);
+			break;
+		default:
+			return;
+	}
+}
+
+function blackAndWhiteHandler(element, color) {
+	element.style.backgroundColor = color;
+	if (color === 'white') {
+		element.classList.remove('active');
 	} else {
-		div.style.backgroundColor = `rgb(${incrementColor(
+		element.classList.add('active');
+	}
+}
+
+function clearGrid() {
+	const div = document.querySelectorAll('.grid');
+	[...div].forEach((d) => {
+		d.style.backgroundColor = 'white';
+		d.classList.remove('active');
+	});
+}
+
+function resetGrid(value) {
+	drawMode = ORIGINAL_MODE;
+	createGrid(value);
+}
+
+function updateSliderValueText(value) {
+	sliderValue.textContent = `${value} x ${value}`;
+}
+
+function randomizeColorHandler(element) {
+	element.style.backgroundColor = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
+	element.classList.add('active');
+}
+
+function gradientColorHandler(element) {
+	const bgColor = element.style.backgroundColor.match(/\d+/g);
+
+	if (!bgColor) {
+		element.style.backgroundColor = `rgb(225,225,225)`;
+	} else {
+		element.style.backgroundColor = `rgb(${incrementColor(
 			bgColor[0]
 		)},${incrementColor(bgColor[1])},${incrementColor(bgColor[2])})`;
 	}
-	div.classList.add('active');
+	element.classList.add('active');
+}
+
+function randomGradientColorHandler(element) {
+	const bgColor = element.style.backgroundColor.match(/\d+/g);
+
+	if (!bgColor) {
+		element.style.backgroundColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+	} else {
+		element.style.backgroundColor = `rgb(${incrementColor(
+			bgColor[0]
+		)},${incrementColor(bgColor[1])},${incrementColor(bgColor[2])})`;
+	}
+	element.classList.add('active');
+}
+
+// Helper functions
+function randomNumber() {
+	return Math.floor(Math.random() * 255);
 }
 
 function incrementColor(color) {
@@ -59,28 +203,4 @@ function incrementColor(color) {
 	return newColor.toString();
 }
 
-function clearGrid() {
-	const div = document.querySelectorAll('.grid');
-	[...div].forEach((d) => d.classList.remove('active'));
-}
-
-function clickHandler() {
-	let newGrid = parseInt(prompt('Enter grid size: '));
-	if (!Number(newGrid) || (newGrid > 100 && newGrid < 0)) {
-		para.textContent = 'Please enter a valid number.';
-		container.appendChild(para);
-		newGrid = prompt('Enter grid size: ');
-	}
-
-	createGrid(newGrid);
-}
-
 createGrid();
-
-function randomNumber() {
-	return Math.floor(Math.random() * 255);
-}
-
-function randomColorGenerator(element) {
-	element.style.backgroundColor = `rgb(${randomNumber()},${randomNumber()},${randomNumber()})`;
-}
